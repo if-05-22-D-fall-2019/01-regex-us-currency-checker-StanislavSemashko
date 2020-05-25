097d23b4eee30cb1a6cd2474362683c27dce617d
StanislavSemashko <43435900+StanislavSemashko@users.noreply.github.com>
2020-05-25 09:48:46 +0200

    Add files via upload

---
+++ UsCurrencyChecker.java
@@ -0,0 +1,15 @@
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UsCurrencyChecker {
+    public static Matcher getMatcher(String usCurrencyString) {
+        Matcher matcher;
+        final String regex = "\\$\\s\\d{3}\\.\\d{2}";
+
+       Pattern pattern = Pattern.compile(regex);
+       matcher = pattern.matcher(usCurrencyString);
+       return matcher;
+    }
+}
