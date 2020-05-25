28c53734fbfeb7eaacc8e5ef516341d7dfe7d922
StanislavSemashko <43435900+StanislavSemashko@users.noreply.github.com>
2020-05-25 10:07:02 +0200

    Update UsCurrencyChecker.java
    10 out of 10 working

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,14 +1,14 @@
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
         Matcher matcher;
-        final String regex = "\\$\\s*\\d*(\\.\\d{1,2})?";
+        final String regex = "\\$\\s*\\d+(\\.\\d{1,2})?";
 
        Pattern pattern = Pattern.compile(regex);
        matcher = pattern.matcher(usCurrencyString);
        return matcher;
     }
 }
+
